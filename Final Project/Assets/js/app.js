8e22f88d467ade72d38717c67349b38b176b9075
Zaki Bernoussi <zbernou@gmail.com>
2017-02-08 19:10:46 -0500

    Final project commits

--- Final Project/Assets/js/app.js
+++ Final Project/Assets/js/app.js
@@ -1,1 +1,17 @@
 console.log("APP JS RUNNING!");
+
+$(document).ready(function() {
+
+	// 1
+	$('#city-type').change(function () {
+
+		// 4
+		var city = $('#city-type').val();
+
+		// 5
+		$('body').removeClass();
+
+		// 6
+		$('body').addClass(city)
+	});
+});
